4a1c639703cc5d99e90d95bf9295dd4800ce8ac4
EmmanuelDV <66754450+EmmanuelDV@users.noreply.github.com>
2020-06-17 16:42:54 -0500

    Create persona.java

---
+++ persona.java
@@ -0,0 +1,42 @@
+package Practica02;
+import javax.swing.JOptionPane;
+public class Persona {
+    int num_trab;
+    static String [] nombrearreglo;
+    static int [] pagobrutoarreglo,essaludarreglo,fonaviarreglo,bonificacionarreglo,sueldoarreglo;
+    
+    
+    
+    public void crearArreglo(){
+        num_trab = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de trabajadores  : "));
+    
+        JOptionPane.showMessageDialog(null,"El arreglo fue creado ");
+    }
+    
+    public void ingresarNombre(){
+        String datos;
+        datos = JOptionPane.showInputDialog("Ingrese nombre de trabajador : ");
+        nombrearreglo = new String[num_trab];
+        JOptionPane.showMessageDialog(null, datos);
+    }
+    
+    public void ingresarPagoBruto(){
+        String datos;
+        datos = JOptionPane.showInputDialog("Ingrese pago bruto : ");
+        pagobrutoarreglo = new int [num_trab];
+        JOptionPane.showMessageDialog(null, datos);   
+    }
+    
+    
+    public void mostrarDatos(){
+        String datos;
+        
+        JOptionPane.showMessageDialog(null,"La lista es:"+nombrearreglo+
+                pagobrutoarreglo+
+                essaludarreglo+fonaviarreglo+
+                bonificacionarreglo);
+ 
+    } 
+    
+    
+}
